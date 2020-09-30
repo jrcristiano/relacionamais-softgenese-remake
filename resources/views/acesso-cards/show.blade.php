cd761d12d5795918fc6f8f05d7e2c3aa30a4533d
Cristiano Junior <cristiano-junior10@outlook.com>
2020-09-30 15:39:40 -0300

    novas correcoes gerais em premiaçoes, sim, de novo --Cristiano

--- resources/views/acesso-cards/show.blade.php
+++ resources/views/acesso-cards/show.blade.php
@@ -45,7 +45,7 @@
                             <td class="text-uppercase">{{ $acessoCard->acesso_card_name }}</td>
                             <td class="spreadsheet_document" >{{ $acessoCard->acesso_card_document }}</td>
                             <td>{{ $acessoCard->acesso_card_number ? $acessoCard->base_acesso_card_proxy : null }}</td>
-                            <td>{{ $acessoCard->acesso_card_number ?? 'EMITIR CARTÃO' }}</td>
+                            <td>{{ $acessoCard->acesso_card_number ? $acessoCard->base_acesso_card_number : 'EMITIR CARTÃO' }}</td>
                             <td>R$ {{ $acessoCard->acesso_card_value_formatted }}</td>
                             <td>
                                 @if (!$acessoCard->acesso_card_chargeback)
