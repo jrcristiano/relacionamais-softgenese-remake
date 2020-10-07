9fe7e3099a84e48d057e21216e53087914034262
Cristiano Junior <cristiano-junior10@outlook.com>
2020-10-07 11:14:05 -0300

    exibicao de texto em negrito quando houver cartoes a serem gerados --Cristiano

--- resources/views/acesso-cards/show.blade.php
+++ resources/views/acesso-cards/show.blade.php
@@ -42,10 +42,10 @@
                 <tbody>
                     @forelse ($acessoCards as $acessoCard)
                         <tr>
-                            <td class="text-uppercase">{{ $acessoCard->acesso_card_name }}</td>
+                            <td class="text-uppercase {{ $acessoCard->acesso_card_generated == 1 ? '' : 'font-weight-bold' }}">{{ $acessoCard->acesso_card_name }}</td>
                             <td class="spreadsheet_document" >{{ $acessoCard->acesso_card_document }}</td>
-                            <td>{{ $acessoCard->acesso_card_number ? $acessoCard->base_acesso_card_proxy : null }}</td>
-                            <td>{{ $acessoCard->acesso_card_number ? $acessoCard->base_acesso_card_number : 'EMITIR CARTÃO' }}</td>
+                            <td class="{{ $acessoCard->acesso_card_generated == 1 ? '' : 'font-weight-bold' }}">{{ $acessoCard->acesso_card_number ? $acessoCard->base_acesso_card_proxy : null }}</td>
+                            <td class="{{ $acessoCard->acesso_card_generated == 1 ? '' : 'font-weight-bold' }}">{{ $acessoCard->acesso_card_number ? $acessoCard->base_acesso_card_number : 'EMITIR CARTÃO' }}</td>
                             <td>R$ {{ $acessoCard->acesso_card_value_formatted }}</td>
                             <td>
                                 @if (!$acessoCard->acesso_card_chargeback)
