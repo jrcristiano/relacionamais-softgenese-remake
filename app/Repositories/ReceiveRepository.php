c77830a71e2592ab455aa41602a74c831b0486d5
Cristiano Junior <cristiano-junior10@outlook.com>
2020-10-20 16:24:53 -0300

    correcao em premiacao com data e status --Cristiano

--- app/Repositories/ReceiveRepository.php
+++ app/Repositories/ReceiveRepository.php
@@ -204,7 +204,7 @@ class ReceiveRepository extends Repository
         }
 
         if ($between[0] && $between[1] && $status == 2 && !$client) {
-            return $query->where(function ($query) use ($between, $status) {
+            return $query->orWhere(function ($query) use ($between, $status) {
                 $query->whereBetween('note_receipt_date', $between)
                     ->where('notes.note_status', $status);
             })
@@ -231,7 +231,7 @@ class ReceiveRepository extends Repository
         }
 
         if ($between[0] && $between[1] && $status == 2 && $client) {
-            return $query->where(function ($query) use ($between, $status, $client) {
+            return $query->orWhere(function ($query) use ($between, $status, $client) {
                 $query->whereBetween('note_receipt_date', $between)
                     ->where('notes.note_status', $status)
                     ->where('demands.demand_client_name', $client);
